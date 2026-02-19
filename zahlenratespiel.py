801d233f8e68a2d0d93ab4d04fdd2b0bec201fd6
yshehneh <yousifshehneh@gmail.com>
2026-02-19 11:39:42 +0100

    Update zahlenratespiel.py

--- zahlenratespiel.py
+++ zahlenratespiel.py
@@ -1,13 +1,28 @@
 import random
 
+siege = 0
+verloren = 0
+
 while True:
 
 
-    print ("Willkommen beim Zahlenratespiel")
-    print("Wähle eine Schwierigkeit:")
-    print("1 = Einfach (1-10, 3 Versuche)")
-    print("2 = Mittel (1-20, 4 Versuche)")
-    print("3 = Schwer (1-50, 5 Versuche)")
+    print ("""
+====================================
+        ZAHLENRATESPIEL
+====================================
+
+Wähle eine Schwierigkeit:
+
+[1] Einfach  (1–10,  3 Versuche)
+[2] Mittel   (1–20,  4 Versuche)
+[3] Schwer   (1–50,  5 Versuche)
+
+====================================   """)
+
+
+
+
+   
 
     auswahl = input("Deine Wahl (1, 2 oder 3): ")
 
@@ -33,16 +48,26 @@ while True:
 
     zahl = random.randint(1, max)
     versuche = 0
+    gewonen = False
 
-    print(f"Zahlenratespiel Level{level}")
 
-    print(f"Errate die Zahl zwischen 1 und {max}")
-    print(f"Hey! Achte darauf, du hast nur {versuche_limit} Versuche.")
+    print (f"""
+           
+------------------------------------
+Zahlenratespiel Level{level}
+
+Errate die Zahl zwischen 1 und {max}
+Hey! Achte darauf, du hast nur {versuche_limit} Versuche
+
+Viel Spaß...
+------------------------------------""")
+
+    
 
 
     while versuche < versuche_limit:
         try:
-            eingabe = int(input("Deine Zahl: "))
+            eingabe = int(input("Gib deine Schätzung ein: "))
         except:
             print("Bitte gib eine Zahl ein!")
             continue
@@ -51,18 +76,50 @@ while True:
 
         if eingabe == zahl:
             print("Richtig! Du hast gewonnen.")
+            siege += 1
+            gewonen = True
             break
         elif eingabe < zahl:
             print("Zu klein, versuch nochmal")
         else:
-            print("Zu groß, versuch nochmal")
+            print("Zu gross, versuch nochmal")
 
-    else:
+    if not gewonen:
+
         print("Leider, du hast verloren. Die Zahl war:", zahl)
+        verloren += 1
+
+
+    statistik = input("Willst du deine Ergebnisse sehen? (ja/nein): ").strip().lower()
+    if statistik == "ja":
+        spiele = siege + verloren
+        if spiele > 0:
+            quote = (siege / spiele) * 100
+        else:
+            quote = 0
+
+        
+
+        print (f"""
+    ========== Deine Ergebnisse ==========
+    Gewonnen   : {siege}
+    Verloren   : {verloren}
+    Gewinnquote: {quote:.2f}%
+    =======================================""")
+           
+
 
 
     nochmal = input("Willst du nochmal spielen? (ja/nein) ").strip().lower()
 
     if nochmal != "ja":
-        print("Ok, wie du willst..")
+        print("Ok, wie du willst..Bye")
         break
+
+
+    
+    
+
+
+
+
